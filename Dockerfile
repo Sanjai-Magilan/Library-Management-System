eef5166e1c065a223e12a92f0cc4644fd16a08b1
Sanjai-Magilan <sanjai6115@gmail.com>
2025-09-26 10:51:51 +0530

    Docker implemented 😎

---
+++ Dockerfile
@@ -0,0 +1,23 @@
+# Use official Node LTS image
+FROM node:18
+
+# Set working directory inside container
+WORKDIR /app
+
+# Copy package.json and package-lock.json first (for better cache)
+COPY package*.json ./
+
+# Install dependencies
+RUN npm install
+
+# Copy the rest of your application code
+COPY . .
+
+#COPY .env ./
+
+
+# Expose the port your server listens on
+EXPOSE 5000
+
+# Default command to run the app
+CMD ["node", "server.js"]
--- models/schema.js
+++ models/schema.js
@@ -4,8 +4,8 @@ const LibSchema = new Schema({
   id: { type: Number, required: true, unique: true },
   name: { type: String, required: true, unique: true }, // if we use uniqe , no need to use index
   author: { type: String, required: true },
-  time: { type: Date, default: () => new Date(Date.now() + 5.5 * 60 * 60 * 1000) },
-  availability: { type: Boolean, default: true }
+  availability: { type: Boolean, default: true },
+  time: { type: Date, default: () => new Date(Date.now() + 5.5 * 60 * 60 * 1000) }
 });
 
 module.exports = mongoose.model("LibBooks", LibSchema);
